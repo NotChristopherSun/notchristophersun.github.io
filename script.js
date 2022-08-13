7d91d2e8a9a86ff4bf1b7eabdd6a380a35153d72
Christopher Sun <notchristophersun@gmail.com>
2022-08-12 23:24:23 -0400

    init

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-function cube() {
+function cube(x = 0, y = 0, z = 0, width = 1, length = 1, height = 1, hue = 210, lightness = 50, alpha = 1) {
     var cube = document.createElement("figure");
-    for (const side of ["top", "left", "right"]) {
-        cube.appendChild(document.createElement("aside"));
-        cube.lastElementChild.classList.add(side);
-    }
+    cube.style.left = (-x + y + (length - width) / 2) * 50 + "px";
+    cube.style.top = (-x * .5 - y * .5 - z + (width + length) / 4 - height) * 57.8 + "px";
+    var top = document.createElement("aside");
+    top.style.width = length * 57.8 + "px";
+    top.style.height = width * 50 + "px";
+    top.style.backgroundColor = `hsla(${hue}, 100%, ${lightness}%, ${alpha})`;
+    top.style.transform = "rotate(210deg) skew(-30deg)";
+    var left = document.createElement("aside");
+    left.style.width = height * 57.8 + "px";
+    left.style.height = length * 50 + "px";
+    left.style.backgroundColor = `hsla(${hue}, 100%, ${lightness - 10}%, ${alpha})`;
+    left.style.transform = "rotate(90deg) skew(-30deg)";
+    var right = document.createElement("aside");
+    right.style.width = width * 57.8 + "px";
+    right.style.height = height * 50 + "px";
+    right.style.backgroundColor = `hsla(${hue}, 100%, ${lightness - 20}%, ${alpha})`;
+    right.style.transform = "rotate(330deg) skew(-30deg)";
+    cube.append(top, left, right);
     document.body.firstElementChild.appendChild(cube);
+}
+
+function init() {
+    cube(0, 0, 0, 1, 1, 1, 200);
 }
